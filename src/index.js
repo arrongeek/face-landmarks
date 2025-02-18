3d6b342f7f2014af38fd067e1c3c4503a0ad3f3a
aaron <aaron@geekstudio.co.kr>
2025-02-18 23:21:10 +0900

    add: 카메라 및 얼굴인식 랜드마크 설정 GUI 비노출 처리

--- src/index.js
+++ src/index.js
@@ -146,6 +146,7 @@ async function app() {
   // Gui content will change depending on which model is in the query string.
   const urlParams = new URLSearchParams(window.location.search);
 
+  // 카메라 및 얼굴인식 랜드마크 설정 GUI 비노출
   await setupDatGui(urlParams);
 
   stats = setupStats();
